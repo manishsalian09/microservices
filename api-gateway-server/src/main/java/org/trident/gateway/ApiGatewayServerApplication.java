5141d08953c8836e2e8a7330548f220b934ce603
manishsalian09 <manishsalian09@gmail.com>
2019-09-08 00:26:15 +0530

    initial commit

---
+++ api-gateway-server/src/main/java/org/trident/gateway/ApiGatewayServerApplication.java
@@ -0,0 +1,17 @@
+package org.trident.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServerApplication.class, args);
+	}
+
+}
